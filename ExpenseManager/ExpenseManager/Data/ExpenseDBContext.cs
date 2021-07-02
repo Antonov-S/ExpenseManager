0ed8a4350e1fc919bf66e4e6a1b46547708ba861
Antonov-S <anto1nov1@gmail.com>
2021-07-02 23:23:49 +0300

    First version
    Tables are not working and the moment.

---
+++ ExpenseManager/ExpenseManager/Data/ExpenseDBContext.cs
@@ -0,0 +1,18 @@
+﻿namespace ExpenseManager.Models
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ExpenseDBContext : DbContext
+    {
+        public virtual DbSet<ExpenseReport> ExpenseReport { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                optionsBuilder.UseSqlServer("Server=localhost;Database=ExpenseReport;Trusted_Connection=True;");
+            }
+        }
+    }
+}
